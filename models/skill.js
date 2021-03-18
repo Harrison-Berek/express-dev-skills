0be345d2f327469f3c2e61510d1069f2ef43596a
Harrison-Berek <hcberek@gmail.com>
2021-03-18 17:47:26 -0600

    add, remove, edit and update

--- models/skill.js
+++ models/skill.js
@@ -1,19 +1,41 @@
 const skills = [
-    {id: 125223, skill: 'HTML', master: true},
-    {id: 127904, skill: 'CSS', master: false},
-    {id: 139608, skill: 'Node', master: false}
-  ];
-  
-  module.exports = {
-    getAll,
-    getOne
-  };
-  
-  function getOne(id) {
-    id = parseInt(id);
-    return skills.find(skill => skill.id === id);
-  }
-  
-  function getAll() {
-    return skills;
-  }
+  {id: 125223, skill: 'HTML', master: true},
+  {id: 127904, skill: 'CSS', master: false},
+  {id: 139608, skill: 'Node', master: false}
+];
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  deleteOne,
+  update
+};
+
+function update(id, skill) {
+  id = parseInt(id);
+  const idx = skills.findIndex(skill => skill.id === id);
+  skill.id = id; 
+  skills.splice(idx, 1, skill);
+}
+
+function deleteOne(id) {
+  id = parseInt(id)
+  const idx = skills.find(skill => skill.id === id);
+  skills.splice(idx, 1);
+};
+
+function create(skill) {
+  skill.id = Date.now() %1000000
+  skill.master = false;
+  skills.push(skill);
+};
+
+function getOne(id) {
+  id = parseInt(id);
+  return skills.find(skill => skill.id === id);
+}
+
+function getAll() {
+  return skills;
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,14 +1,25 @@
 var express = require('express');
+const skills = require('../controllers/skills');
 var router = express.Router();
 // Require the controller for the todos resource
 var skillsCtrl = require('../controllers/skills');
 
 // All actual paths begin with "/todos"
 
-/* GET /todos     --- "index" action/functionality */
+/* GET /skills     --- "index" action/functionality */
 router.get('/', skillsCtrl.index);
-// GET /todos/:id
+// GET/skills/new
+router.get('/new', skillsCtrl.new);
+// GET /skills/:id
 router.get('/:id', skillsCtrl.show);
+// POST /skills
+router.post('/', skillsCtrl.create);
+// DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
+// Edit /skills/:id/edit
+router.get('/:id/edit', skillsCtrl.edit);
+//Update /skills/:id
+router.put('/:id', skillsCtrl.update); 
 
 
 module.exports = router;
